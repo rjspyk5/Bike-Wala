5b6b0b622c189ef0cc4b151b51f4bbfc0d8f33d2
rjspyk5 <rjspyk5@gmail.com>
2024-01-20 19:52:48 +0600

    Intial commit for connecting project to github

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-center text-red-500">Hello Wrld</h1>
+    </>
+  )
+}
+
+export default App

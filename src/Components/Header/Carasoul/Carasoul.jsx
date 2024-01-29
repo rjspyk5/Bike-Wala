a3f06a6c39f4e0f8d293e74169d9dad778bf9af6
rjspyk5 <rjspyk5@gmail.com>
2024-01-29 17:21:12 +0600

    Banner and carasoul section complete

--- src/Components/Header/Carasoul/Carasoul.jsx
+++ src/Components/Header/Carasoul/Carasoul.jsx
@@ -10,12 +10,12 @@ export const Carasoul = () => {
   return (
     <div className='carousel w-full bg-[#264653]'>
       {/* slider one */}
-      <div id='slide1' className='carousel-item relative w-full'>
+      <div id='slide1' className='carousel-item  relative w-full'>
         <Hero
           img={bike1}
-          content='lorem10
+          content='Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
         '
-          header='test'
+          header='Honda CBR 300R'
         />
 
         <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
@@ -31,9 +31,9 @@ export const Carasoul = () => {
       <div id='slide2' className='carousel-item relative w-full'>
         <Hero
           img={bike2}
-          content='lorem10
+          content='Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
         '
-          header='test'
+          header='2021 Honda CBR500R'
         />
         <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
           <a href='#slide1' className='btn btn-circle'>
@@ -48,9 +48,9 @@ export const Carasoul = () => {
       <div id='slide3' className='carousel-item relative w-full'>
         <Hero
           img={bike3}
-          content='lorem10
+          content='Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
         '
-          header='test'
+          header='Ducati XDIAVEL S-73'
         />
         <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
           <a href='#slide2' className='btn btn-circle'>
@@ -65,9 +65,9 @@ export const Carasoul = () => {
       <div id='slide4' className='carousel-item relative w-full'>
         <Hero
           img={bike4}
-          content='lorem10
+          content='Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
         '
-          header='test'
+          header='Honda R15 v-2'
         />
         <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
           <a href='#slide3' className='btn btn-circle'>
--- src/Components/Header/Carasoul/Hero.jsx
+++ src/Components/Header/Carasoul/Hero.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 export const Hero = props => {
   return (
     <div className='w-full '>
-      <div className='hero-content flex-col justify-around lg:flex-row-reverse'>
+      <div className='hero-content flex-col justify-between lg:flex-row-reverse'>
         <img src={props.img} />
-        <div>
+        <div className='*:text-white max-w-[456px]'>
+          <p className='font-normal text-[14px]'>
+            Computer-controlled digital transistorized with electronic advance
+          </p>
           <h1 className='text-5xl font-bold'>{props.header}</h1>
-          <p className='py-6'>{props.content}</p>
-          <button className='btn btn-primary'>Get Started</button>
+          <p className='py-6 font-normal'>{props.content}</p>
+          <button className='btn bg-[#E76F51] '>Purchage</button>
         </div>
       </div>
     </div>
